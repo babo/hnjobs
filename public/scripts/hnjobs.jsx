feba1a8532b13caaa077240eec4e119f1c9bfa12
Attila Babo <attila.babo@gmail.com>
2015-04-05 22:03:28 +0200

    remove some more copy-pasted code

--- public/scripts/hnjobs.jsx
+++ public/scripts/hnjobs.jsx
@@ -80,23 +80,6 @@ CommentBox = React.createClass({
             setInterval(this.loadCommentsFromServer, this.props.pollInterval);
         }
     },
-    handleCommentSubmit: function (comment) {
-        var comments = this.state.data,
-            newComments = comments.concat([comment]);
-        this.setState({data: newComments});
-        $.ajax({
-            url: this.props.url,
-            dataType: 'json',
-            type: 'POST',
-            data: comment,
-            success: function (data) {
-                this.setState({data: data});
-            }.bind(this),
-            error: function (xhr, status, err) {
-                console.error(this.props.url, status, err.toString());
-            }.bind(this)
-        });
-    },
     render: function () {
         return (
             <div className='commentBox'>
