4220c34352bc217de001189dca7bb2e076ebdfe1
Attila Babo <attila.babo@gmail.com>
2016-02-07 01:46:17 +0100

    Make it easy to retrieve and serve a given date or thread id

--- hn.py
+++ hn.py
@@ -9,6 +9,8 @@
 """
 Retrieving jobs from the monthly "Ask HN: Who is hiring?" on Hacker News
 """
+import argparse
+import datetime
 import json
 import time
 
@@ -16,19 +18,19 @@ from contextlib import closing
 from urllib.request import urlopen
 
 import rethinkdb
+from pytz import timezone
 
-
-IDS = ['11012044', '10822019', '10655740', '10492086', '10311580', '10152809', '9996333', '9639001', '9471287', '9303396', '9127232', '8980047']
-MAIN_ID = IDS[0]
 TEMPLATE = 'https://hacker-news.firebaseio.com/v0/item/{}.json'
 WHO_IS_HIRING = 'https://hacker-news.firebaseio.com/v0/user/whoishiring.json'
 
 DB_HOST = 'localhost'
 DB_PORT = 28015
 DB_DATABASE = 'hnjobs'
+DB_THREAD_IDS = 'threads'
 MAX_RETRIES = 5
 
 class FireBaseException(Exception):
+    "FireBase connection error"
     pass
 
 def read_firebase(hn_url):
@@ -42,20 +44,11 @@ def read_firebase(hn_url):
                 else:
                     raise FireBaseException(x.code)
     except Exception as error:
-        print(hn_id, type(error), error)
+        print(hn_url, type(error), error)
+        raise
 
-def latest_thread():
-    submitted = map(int, read_firebase(WHO_IS_HIRING)['submitted'])
-
-    # check submissions in cronological order
-    for hn_id in sorted(submitted, reverse=True):
-        post_data = read_firebase(TEMPLATE.format(hn_id))
-        if 'title' in post_data and 'Ask HN: Who is hiring?' in post_data['title']:
-            print(post_data['title'])
-            return hn_id
-
-def get_all(start_id=None, seen=None):
-    ids = [start_id or MAIN_ID]
+def get_all(main_id, seen=None):
+    ids = [main_id]
     seen = set(seen or []).difference(ids)
 
     failed = []
@@ -81,32 +74,108 @@ def get_all(start_id=None, seen=None):
     if failed:
         print('Failed', failed)
 
+def get_date(year=None, month=None):
+    # It's published on the first day of the month at 9am EST
+    est = timezone('US/Eastern')
+    now = datetime.datetime.now(tz=est)
+    if year or month:
+        if year is None:
+            year = now.year
+        if month is None:
+            month = now.month
+        when = datetime.datetime(year or now.year, month or now.month, 1, 9, 0, 0, 0, est)
+    elif now.day == 1 and now.hour < 9:
+        print("It's not published yet, check back in {} hours".format(9 - now.hour))
+        when = now - datetime.timedelta(1)
+    else:
+        when = now
+    return when
+
+def date2key(when):
+    return when.strftime('%Y-%m')
+
+def validate_thread_id(connection, hn_id):
+    cursor = rethinkdb.table(DB_THREAD_IDS).filter({'thread_id': hn_id}).run(connection)
+    if cursor.items:
+        key = cursor.next()['id']
+        cursor.close()
+        when = get_date(*[int(x) for x in key.split('-')])
+    else:
+        post_data = read_firebase(TEMPLATE.format(hn_id))
+        when = datetime.datetime.fromtimestamp(post_data['time'])
+        thread_id = {'id': date2key(when), 'thread_id': hn_id}
+        rethinkdb.table(DB_THREAD_IDS).insert(thread_id).run(connection)
+    return hn_id
+
+def get_latest_thread(connection, year=None, month=None):
+    when = get_date(year, month)
+    key = date2key(when)
+    thread_id = rethinkdb.table(DB_THREAD_IDS).get(key).run(connection)
+    if thread_id is None:
+        monthly_title = when.strftime('Ask HN: Who is hiring? (%B %Y)')
+        submitted = [int(x) for x in read_firebase(WHO_IS_HIRING)['submitted']]
+
+        # check submissions in cronological order
+        for hn_id in sorted(submitted, reverse=True):
+            post_data = read_firebase(TEMPLATE.format(hn_id))
+            print(post_data['title'] if 'title' in post_data else post_data)
+            if 'title' in post_data and monthly_title in post_data['title']:
+                thread_id = {'id': key, 'thread_id': hn_id}
+                rethinkdb.table(DB_THREAD_IDS).insert(thread_id).run(connection)
+                break
+        else:
+            raise Exception('There is no "{}" found'.format(monthly_title))
+
+    return thread_id['thread_id']
+
+def get_main_id(connection, args):
+    args = check_args()
+
+    if args.thread_id:
+        return validate_thread_id(connection, args.thread_id)
+    else:
+        return get_latest_thread(connection, args.year, args.month)
+
 def init_db():
-    c = rethinkdb.connect(host=DB_HOST, port=DB_PORT)
-    if DB_DATABASE not in rethinkdb.db_list().run(c):
-        x = rethinkdb.db_create(DB_DATABASE).run(c)
+    connection = rethinkdb.connect(host=DB_HOST, port=DB_PORT)
+    if DB_DATABASE not in rethinkdb.db_list().run(connection):
+        x = rethinkdb.db_create(DB_DATABASE).run(connection)
         if x.get('dbs_created', 0) != 1:
             raise Exception('Unable to create database')
     c = rethinkdb.connect(host=DB_HOST, port=DB_PORT, db=DB_DATABASE)
 
-    if DB_THREAD_IDS not in rethinkdb.table_list().run(c):
-        x = rethinkdb.table_create(DB_THREAD_IDS).run(c)
+    if DB_THREAD_IDS not in rethinkdb.table_list().run(connection):
+        x = rethinkdb.table_create(DB_THREAD_IDS).run(connection)
         if x.get('tables_created', 0) != 1:
-            raise Exception('Unable to create table {}'.format(MAIN_ID))
+            raise Exception('Unable to create table {}'.format(DB_THREAD_IDS))
 
     return c
 
-def main():
-    c = init_db()
-    table = rethinkdb.table(MAIN_ID)
+def check_args():
+    parser = argparse.ArgumentParser(description='Read monthly who is hiring thread from hacker new')
+    parser.add_argument('-y', '--year', type=int, dest='year', action='store', default=None, help='from 2012, default is the current')
+    parser.add_argument('-m', '--month', type=int, dest='month', action='store', default=None, help='as an integer, default is the current')
+    parser.add_argument('-t', '--thread', type=int, dest='thread_id', action='store', default=None, help='thread id as an integer, default is the current')
 
-    cur = table.with_fields('id').run(c)
+    return parser.parse_args()
+
+def get_connection():
+    args = check_args()
+    connection = init_db()
+    main_id = get_main_id(connection, args)
+    table = rethinkdb.table(str(main_id))
+    return connection, table, main_id
+
+def main():
+    connection, table, main_id = get_connection()
+
+    cur = table.with_fields('id').run(connection)
     seen = set([x['id'] for x in cur])
-    for data in get_all(seen=seen):
+    for data in get_all(main_id, seen=seen):
         jid = data['id']
         if jid not in seen:
             data['cool'] = 1
-            x = table.insert(data).run(c)
+            x = table.insert(data).run(connection)
             if x.get('inserted', 0) != 1:
                 print('Fail to add {} {}'.format(jid, x))
             else:
--- server.py
+++ server.py
@@ -16,22 +16,16 @@ import rethinkdb
 
 from flask import Flask, Response, request
 
-from hn import MAIN_ID
+import hn
 
 LIMIT = 700
-DB_HOST = 'localhost'
-DB_PORT = 28015
-DB_DATABASE = 'hnjobs'
 
 app = Flask(__name__, static_url_path='', static_folder='public')
 app.add_url_rule('/', 'root', lambda: app.send_static_file('./index.html'))
 
 @app.route('/hnjobs', methods=['GET'])
 def comments_handler():
-    c = rethinkdb.connect(host=DB_HOST, port=DB_PORT, db=DB_DATABASE)
-    table = rethinkdb.table(MAIN_ID)
-
-    cursor = table.filter({'parent': int(MAIN_ID), 'type': 'comment'})
+    cursor = app.table.filter({'parent': app.main_id, 'type': 'comment'})
 
     navmode = request.args.get('navmode', '0')
     if navmode == '2':
@@ -47,46 +41,41 @@ def comments_handler():
     if jobfilter:
         cursor = cursor.filter(rethinkdb.row['text'].match(r'(?i){}'.format(jobfilter)))
 
-    jobs = list(cursor.limit(LIMIT).run(c))
+    jobs = list(cursor.limit(LIMIT).run(app.connection))
 
     return Response(json.dumps(jobs), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
 
 @app.route('/hnjobs/latest', methods=['GET'])
 def latest_handler():
-    c = rethinkdb.connect(host=DB_HOST, port=DB_PORT, db=DB_DATABASE)
-    table = rethinkdb.table(MAIN_ID)
-
-    counters = table.group('cool').count().run(c)
-    rtv = {'url': 'https://news.ycombinator.com/item?id={}'.format(MAIN_ID),
-        'total': table.count().run(c),
-        'date': table.get(MAIN_ID).run(c)['time'],
-        'latest': table.filter({'parent': int(MAIN_ID)}).max('time').run(c)['time'],
+    counters = app.table.group('cool').count().run(app.connection)
+    rtv = { \
+        'url': 'https://news.ycombinator.com/item?id={}'.format(app.main_id),
+        'date': app.table.get(app.main_id).run(app.connection)['time'],
+        'latest': app.table.filter({'parent': app.main_id}).max('time').run(app.connection)['time'],
         'liked': counters[2],
         'disliked': counters[0],
         'unclassified': counters[1],
-        'total': sum(counters.values())
-    }
+        'total': sum(counters.values())}
 
     return Response(json.dumps(rtv), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
 
 @app.route('/hnjobs/update/<jobid>', methods=['DELETE', 'POST'])
 def hide_a_job_handler(jobid):
-    c = rethinkdb.connect(host=DB_HOST, port=DB_PORT, db=DB_DATABASE)
-    table = rethinkdb.table(MAIN_ID)
 
     success = False
     if request.method == 'DELETE':
-        rtv = table.get(jobid).update({'cool': 0}).run(c)
+        rtv = app.table.get(jobid).update({'cool': 0}).run(app.connection)
         success = rtv.get('skipped', 1) == 0
         app.logger.debug('Hide {} {}'.format(jobid, rtv))
     elif request.method == 'POST':
-        rtv = table.get(jobid).update({'cool': rethinkdb.row['cool'] + 1}).run(c)
+        rtv = app.table.get(jobid).update({'cool': rethinkdb.row['cool'] + 1}).run(app.connection)
         success = rtv.get('skipped', 1) == 0
         app.logger.debug('Updated {} {}'.format(jobid, rtv))
     rtv = {'success': success}
     return Response(json.dumps(rtv), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
 
 def main():
+    app.connection, app.table, app.main_id = hn.get_connection()
     app.run(port=int(os.environ.get('PORT', 3000)), debug=True)
 
 if __name__ == '__main__':
